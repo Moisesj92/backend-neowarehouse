bce2ceb4da6a36e8f6e0e63bbcdbfd47fdc3b6f8
Arsenio Jimenez <moises.jimenez92@gmail.com>
2025-12-24 15:02:35 -0300

    feat: add express and typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    mensaje: "¡Hola Mundo!",
+    tecnologia: "Express + TypeScript + Node 22",
+    status: "OK",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
